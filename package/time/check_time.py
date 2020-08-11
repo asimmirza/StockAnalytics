ce4019b0755333dc077a7dc282e9f73b5bf95069
asimmirza <asim.j.mirza@gmail.com>
2020-08-11 23:18:25 +0530

    Creating Time Class

---
+++ package/time/check_time.py
@@ -0,0 +1,13 @@
+import pytz
+from datetime import datetime, timedelta
+from pytz import timezone
+
+class TimeIndia:
+    def getCurrentTime():
+        time_india = pytz.timezone("Asia/Calcutta")
+        act_time = datetime.now(time_india)
+        return act_time
+
+
+# ti =  TimeIndia.getCurrentTime()
+# print(ti))
