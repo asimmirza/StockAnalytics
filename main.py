c1ffc23ad513ae41c576d60120c028b98fa98da9
asimmirza <asim.j.mirza@gmail.com>
2020-08-12 20:15:46 +0530

    Fixing Market Close Time Issue

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ import time
 if __name__ == '__main__':
 
     flag_mkt_open = True
+    temp_time = TimeIndia.getCurrentTime()
+    market_close_time = temp_time.replace(hour=15, minute=30, second=0, microsecond=0)
+    print(market_close_time)
     while flag_mkt_open:
         ct = TimeIndia.getCurrentTime()
         call_users = GCPUtility.GCPStorage
         users = call_users.readCSV('stock-predictor-bucket',"register_user.csv")
         pp.readUsers(users)
 
-
+        print("Check Time")
         # Checking the Market Close Time
-        if ct.hour>=15 and ct.minute >=30 :
+        if ct >= market_close_time :
             print("Market Closed")
             flag_mkt_open = False
-
-        time.sleep(300)
+            break
+        time.sleep(60)
 
