d3249a10d4a2a2bc2c2705f3f6657484455fc7ad
asimmirza <asim.j.mirza@gmail.com>
2020-08-12 13:59:10 +0530

    Added Code for GCP and multiprocessing

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from package.time.check_time import TimeIndia
+from package.GCPService import GCPUtility
+from package.ParallelProcess.ParallelOption import ParallelProcessing as pp
+import time
+
 
 if __name__ == '__main__':
 
     flag_mkt_open = True
     while flag_mkt_open:
         ct = TimeIndia.getCurrentTime()
+        call_users = GCPUtility.GCPStorage
+        users = call_users.readCSV('stock-predictor-bucket',"register_user.csv")
+        pp.readUsers(users)
+
 
         # Checking the Market Close Time
         if ct.hour>=15 and ct.minute >=30 :
             print("Market Closed")
             flag_mkt_open = False
+
+        time.sleep(300)
 
